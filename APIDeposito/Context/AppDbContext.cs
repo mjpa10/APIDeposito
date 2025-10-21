a668e94b845e9cd0eb0642cb1d55ad4f37874773
Matheus <m.theus.jose.pereira@gmail.com>
2025-10-21 14:58:56 -0300

    Contexto DB em andamento :construction:

---
+++ APIDeposito/Context/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using APIDeposito.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIDeposito.Context;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+
+    }
+
+    public DbSet<Cliente> Clientes { get; set; }
+    public DbSet<ItemVenda> ItenVendas { get; set; }
+    public DbSet<Produto> Produtos { get; set; }
+    public DbSet<Venda> Vendas { get; set; }
+    
+}
